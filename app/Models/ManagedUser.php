b5753f47d65f5fea0a3740bab40138f1de6b95c3
Tom Hine <tomhine792@gmail.com>
2022-07-04 21:23:35 +0100

    made managed user properties fillable

--- app/Models/ManagedUser.php
+++ app/Models/ManagedUser.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class ManagedUser extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'job_title',
+        'contact_number',
+    ];
 }
