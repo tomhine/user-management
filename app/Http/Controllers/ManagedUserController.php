cd65594d04150d67d5361b24d7d145db167d2a20
Tom Hine <tomhine792@gmail.com>
2022-07-04 22:00:47 +0100

    added user create and store routes and functions

--- app/Http/Controllers/ManagedUserController.php
+++ app/Http/Controllers/ManagedUserController.php
@@ -10,12 +10,33 @@ class ManagedUserController extends Controller
     /**
      * Get all users and return with the index view
      *
-     * @return Illuminate\Contracts\View\View
+     * @return View
      */
     public function index()
     {
         return view('index', [
             'users' => ManagedUser::all(),
         ]);
+    }
+
+    /**
+     * Return the new user creation page
+     *
+     * @return View
+     */
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    /**
+     * Validates incoming request to store a new user & stores if validation passes
+     *
+     * @param Request $request
+     * @return redirect
+     */
+    public function store(Request $request)
+    {
+        dd($request);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(ManagedUserController::class)->middleware(['auth'])->group(function () {
     Route::get('/', 'index')->name('user.index');
+    Route::get('/new-user', 'create')->name('user.create');
+    Route::post('/new-user', 'store')->name('user.store');
 });
 
 require __DIR__.'/auth.php';
