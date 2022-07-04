6abb124e7cb1d82e4f362eea5bcb3363366c6ad2
Tom Hine <tomhine792@gmail.com>
2022-07-04 21:22:46 +0100

    added cols to managed user table

--- database/migrations/2022_07_04_191159_create_managed_users_table.php
+++ database/migrations/2022_07_04_191159_create_managed_users_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('managed_users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('job_title');
+            $table->string('contact_number');
             $table->timestamps();
         });
     }
