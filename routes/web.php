372d1614c8d5f297a040038f668f55b2f9bb8af5
Tom Hine <tomhine792@gmail.com>
2022-07-05 01:49:18 +0100

    updated uri's and added for update and delete

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(ManagedUserController::class)->middleware(['auth'])->group(function () {
     Route::get('/', 'index')->name('user.index');
-    Route::get('/new-user', 'create')->name('user.create');
-    Route::post('/new-user', 'store')->name('user.store');
+    Route::get('/user/create', 'create')->name('user.create');
+    Route::post('/user', 'store')->name('user.store');
+    Route::get('/user/{user}/edit', 'edit')->name('user.edit');
+    Route::put('/user/{user}', 'update')->name('user.update');
+    Route::delete('/user/{user}', 'destroy')->name('user.destroy');
 });
 
 require __DIR__.'/auth.php';
